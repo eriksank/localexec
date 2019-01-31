2050b254df7361c87dfc2226615549ede031125e
Erik Poupaert <erik@sankuru.biz>
2019-01-31 16:58:54 +0700

    commit-Thu Jan 31 16:58:54 +07 2019

---
+++ admin/_clean.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------
+#      admin
+#      Written by Erik Poupaert, Cambodia
+#      (c) 2018
+#      Licensed under the LGPL
+#-------------------------------------------------------
+PROGRAM=$(cat PROGRAM)
+VERSION=$(cat VERSION)
+rm -f *.src.rock
+rm -f *.tar.gz
+rm -f *.rockspec
+
